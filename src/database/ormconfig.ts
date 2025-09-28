4b600bf879276ade5667de338e70a9a526d60637
glish0 <dylanemempouza02@gmail.com>
2025-09-28 06:43:07 +0100

    first commit and connection to the database pg done

---
+++ src/database/ormconfig.ts
@@ -0,0 +1,13 @@
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'devuser',
+  password: '1234',
+  database: 'blog'
+}
+
+export default config
