187d381d15903d23befdfc2aa7b203edcc334aad
glish0 <dylanemempouza02@gmail.com>
2025-09-28 09:18:23 +0100

    created user dto initiated

---
+++ src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { UserService } from "@/user/user.service";
+import { CreateUserDto } from "./dto/create-user.dto";
+
+
+@Controller('users')
+export class UserController {
+  constructor  (private readonly userService : UserService) {}
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto ) : Promise<any> {
+    return await this.userService.createUser(createUserDto)
+  }
+}
